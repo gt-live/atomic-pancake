f1fd1de6eece051a177c7692ba8c1e2be9f479f9
gt-live <127173976+gt-live@users.noreply.github.com>
2023-09-21 12:04:02 +0800

    Create source.sh

---
+++ source.sh
@@ -0,0 +1,10 @@
+aws_access_key_id=AKIXXXXXXXXXXXXXXXX
+aws_secret_access_key=NSq6XXXXXXXXXXXXXXXXXX
+
+export PATH=$PATH:~/.tgenv/bin:~/.tfenv/bin
+export AWS_ACCESS_KEY_ID=AKIXXXXXXXXXXXXXXXX
+export AWS_SECRET_ACCESS_KEY="NSq6XXXXXXXXXXXXXXXXXX"
+
+export PROJECT_ROOT=`pwd`
+export TF_VAR_project_root="$PROJECT_ROOT"
+export PATH=$PATH:$PROJECT_ROOT/tools
